dd53a0dacc8190a24411fb2491a86c9d18e4dd9e
ChiaChiKuo <guoj9417@gmail.com>
2026-06-19 14:32:34 +0800

    簡單後端生成

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Practicing_project
-專案練習，提升自己的實力最重要!!!
---
+++ app.py
@@ -0,0 +1,17 @@
+# 匯入 Flask 套件
+from flask import Flask
+
+# 建立 Flask 應用程式
+app = Flask(__name__)
+
+# 當使用者進入首頁("/")時，執行這個函式
+@app.route("/")
+def home():
+    # 回傳文字給瀏覽器
+    return "Hello Flask!"
+
+# 啟動伺服器
+if __name__ == "__main__":
+    # debug=True 代表開發模式
+    # 程式修改後會自動重新啟動
+    app.run(debug=True)
